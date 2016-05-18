9833823db6032e5c13a0fe4fcdf2bea6258dc043
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-05-18 12:32:24 -0400

    static

--- static/js/init.js
+++ static/js/init.js
@@ -1,12 +1,3 @@
 $(document).ready(function() {
-	$('.modal-trigger').leanModal();
-	$('.scrollspy').scrollSpy();
 	$('.button-collapse').sideNav();
-	$('.typed').typed({
-		strings: ["Subtitle: "],
-		typeSpeed: 100,
-		callback: function() {
-			scramble(['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5'])
-		}
-	});
 });
